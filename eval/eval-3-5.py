fb81952e9df92c3f68bbff62cbc405113241ec2d
Mathsion <2646769409@qq.com>
2025-04-22 11:38:32 +0800

    Create eval-3-5.py

---
+++ eval/eval-3-5.py
@@ -0,0 +1,76 @@
+import json
+import argparse
+import os
+from prettytable import PrettyTable
+import pandas as pd
+from openpyxl.styles import PatternFill, Font, Alignment
+from tqdm import tqdm
+import multiprocessing
+from functools import partial
+import re
+
+import re
+
+def extract_option_labels(text, options='ABCD'):
+    if not isinstance(text, str) or not isinstance(options, str):
+        return "error"
+
+    patterns = [
+        r'(?i)\*\*\s*Answer\s*:?[\s\n]*\*\*?(allowed|rejected)\*?',
+
+        r'(?i)Answer\s*:?[\s\n]*(allowed|rejected)',
+
+        r'(?i)(?:\\textbf{|\\mathbf{|\\mathrm{|\\text{)?(allowed|rejected)}?',
+    ]
+
+    for pattern in patterns:
+        match = re.search(pattern, text, re.IGNORECASE)
+        if match:
+            return match.group(1).strip("**")
+
+    return "error"
+
+def score(file_name,output):
+    df = pd.read_json(file_name)
+    instructions = []
+    answers = []
+    model_answers = []
+    scores = []
+
+    df_iter = tqdm(df.iterrows(), total=len(df))
+    for index, row in df_iter:
+        answer = row["label"]
+        model_output = extract_option_labels(row["response"], options='ABCD')
+        model_output = model_output.replace(" ", "")
+        if model_output.strip() != "error" and model_output != "" and model_output is not None:
+            if model_output.strip() == answer:
+                scores.append(1)
+            else:
+                scores.append(0)
+            model_answers.append(model_output)
+        elif model_output == 'error':
+            scores.append("error")
+            model_answers.append(row["response"])
+        else:
+            scores.append(0)
+            model_answers.append(model_output)
+        answers.append(answer)
+        instructions.append(row["instruction"])
+        df_iter.set_description(f"score: {len(list(filter(lambda x: x == 1, scores)))/len(df):.4f}")
+    new_df = pd.DataFrame({
+        "instruction": instructions,
+        "response": model_answers,
+        "label": answers,
+        "score": scores,
+    })
+    new_df.to_excel(output)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--file_name', required=True, type=str)
+    parser.add_argument('--output', required=True, type=str)
+    args = parser.parse_args()
+
+    score(args.file_name,args.output)
